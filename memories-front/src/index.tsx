37725284d712a7b1ad79274129f0f510b084199b
LYS(Mac) <sella45@naver.com>
2025-03-06 10:03:52 +0900

    it아카데미

--- memories-front/src/index.tsx
+++ memories-front/src/index.tsx
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 
-
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
@@ -12,5 +11,3 @@ root.render(
     <App />
   </React.StrictMode>
 );
-
-
