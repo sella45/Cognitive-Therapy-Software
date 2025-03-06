3e310a1e51ebbcf500252e92283c499054365000
LYS(Mac) <sella45@naver.com>
2025-03-06 12:32:11 +0900

    it아카데미

--- memories-front/src/App.tsx
+++ memories-front/src/App.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { Route, Routes } from 'react-router';
 
 import './App.css';
+import Layout from './layouts/Layout';
 
 // Router 구성
 // - /auth : 로그인 및 회원가입 페이지
@@ -22,27 +23,31 @@ import './App.css';
 function App() {
   return (
     <Routes>
-      <Route path={'/auth'} element={<>로그인 회원가입 페이지</>} />
+      <Route path={'auth'} element={<>로그인 회원가입 페이지</>} />
 
-      <Route path={'/main'} element={<>메인 페이지</>} />
+      <Route element={<Layout />}>
+        <Route path={'main'} element={<>메인 페이지</>} />
 
-      <Route path={'/memory-test'}>
-        <Route index element={<>기억력 검사 페이지</>} />
-        <Route path={'complete'} element={<>기억력 검사 완료 페이지</>} />
-      </Route>
-
-      <Route path={'/concentration-test'}>
-        <Route index element={<>집중력 검사 페이지</>} />
-        <Route path={'complete'} element={<>집중력 검사 완료 페이지</>} />
-      </Route>
-
-      <Route path={'/diary'}>
-        <Route index element={<>일기 메인 페이지</>} />
-        <Route path={'write'} element={<>일기 작성 페이지</>} />
-        <Route path={':diaryNumber'}>
-          <Route index element={<>일기 보기 페이지</>} />
-          <Route path={'update'} element={<>일기 수정 페이지</>} />
+        <Route path={'memory-test'}>
+          <Route index element={<>기억력 검사 페이지</>} />
+          <Route path={'complete'} element={<>기억력 검사 완료 페이지</>} />
         </Route>
+
+        <Route path={'concentration-test'}>
+          <Route index element={<>집중력 검사 페이지</>} />
+          <Route path={'complete'} element={<>집중력 검사 완료 페이지</>} />
+        </Route>
+
+        <Route path={'diary'}>
+          <Route index element={<>일기 메인 페이지</>} />
+          <Route path={'write'} element={<>일기 작성 페이지</>} />
+          <Route path={':diaryNumber'}>
+            <Route index element={<>일기 보기 페이지</>} />
+            <Route path={'update'} element={<>일기 수정 페이지</>} />
+          </Route>
+        </Route>
+
+        <Route path={'*'} element={<>404 페이지</>} />
       </Route>
     </Routes>
   );
