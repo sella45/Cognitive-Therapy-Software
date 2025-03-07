6f64006f01aff5999c1af7de6ddc1ed748c8a11a
LYS(Mac) <sella45@naver.com>
2025-03-07 10:52:11 +0900

    it아카데미

---
+++ memories-front/src/components/InputBox/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './style.css';
+
+// component: 공통 인풋 박스 컴포넌트 //
+export default function InputBox() {
+    // render: 공통 인풋 박스 컴포넌트 //
+
+  return (
+    <div>
+        index
+    </div>
+  )
+}
