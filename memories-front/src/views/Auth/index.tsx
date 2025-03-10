39847b25079eb9ddf4ceee97eb940b7dd2047825
LYS(Mac) <sella45@naver.com>
2025-03-10 11:54:31 +0900

    it아카데미

--- memories-front/src/views/Auth/index.tsx
+++ memories-front/src/views/Auth/index.tsx
@@ -3,20 +3,30 @@ import React, { useState } from 'react';
 import SignIn from './SignIn';
 import SignUp from './SignUp';
 
+import { AuthPage } from 'src/types/aliases';
+
 import './style.css';
 
 // component: 로그인 회원가입 화면 컴포넌트 //
 export default function Auth() {
 
   // state: 페이지 상태 //
-  const [page, setPage] = useState<'sign-in' | 'sign-up'>('sign-in');
+  const [page, setPage] = useState<AuthPage>('sign-up');
+
+  // event handler: 페이지 변경 이벤트 처리 //
+  const onPageChangeHandler = (page: AuthPage) => {
+    setPage(page)
+  };
 
   // render: 로그인 회원가입 화면 컴포넌트 렌더링 //
   return (
     <div id='auth-wrapper'>
       <div className='auth-side-image'></div>
       <div className='auth-box'>
-        {page === 'sign-in' ? <SignIn /> : <SignUp/>}
+        {page === 'sign-in' ? 
+        <SignIn onPageChange={onPageChangeHandler} /> : 
+        <SignUp onPageChange={onPageChangeHandler} />
+        }
       </div>
     </div>
   )
